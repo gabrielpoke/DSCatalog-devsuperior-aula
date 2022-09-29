dd44773c5b99d5c1b46f99cc8467b4741783ebfc
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-09-28 21:58:59 -0300

    CRUD de usuários PARTE 2

--- BACKEND/src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ BACKEND/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -9,12 +9,14 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.ResourceAccessException;
 
 import com.devsuperior.dscatalog.dto.RoleDTO;
 import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
 import com.devsuperior.dscatalog.entities.Role;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.RoleRepository;
@@ -22,6 +24,9 @@ import com.devsuperior.dscatalog.repositories.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository repository;
@@ -48,11 +53,13 @@ public class UserService {
 	}
 	
 	@Transactional
-	public UserDTO insert(UserDTO dto) {
+	public UserDTO insert(UserInsertDTO dto) {
 		
 		User entity = new User();
 		
 		copyDtoToEntity(dto, entity);
+		
+		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
 		
 		entity = repository.save(entity);
 		
