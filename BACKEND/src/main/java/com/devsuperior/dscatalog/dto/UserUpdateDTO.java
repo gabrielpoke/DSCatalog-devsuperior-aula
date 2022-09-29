84cb1f863ceb0c4987dc6f40d5668d0526413cac
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-09-29 11:29:48 -0300

    UserUpdateValidator PARTE 2

--- BACKEND/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
+++ BACKEND/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -8,6 +8,4 @@ public class UserUpdateDTO extends UserDTO{
 	
 	private static final long serialVersionUID = 1L;
 	
-	public UserUpdateDTO(){}
-	
 }
--- BACKEND/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ BACKEND/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -56,7 +56,7 @@ public class UserResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@Valid @PathVariable Long id, @RequestBody UserUpdateDTO dto){
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto){
 		UserDTO newDto = service.update(id, dto);
 		
 		return ResponseEntity.ok().body(newDto);
