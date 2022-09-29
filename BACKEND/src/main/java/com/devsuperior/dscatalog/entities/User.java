7c0b2756ff0c2c2dab176bcae52968f3a8a5d70c
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-09-29 12:55:26 -0300

    Implementando checklist do Spring Security

--- BACKEND/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ BACKEND/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,9 +19,13 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
@@ -53,52 +60,42 @@ public class User implements Serializable{
 		this.password = password;
 	}
 	
-
 	public Long getId() {
 		return id;
 	}
 	
-
 	public void setId(Long id) {
 		this.id = id;
 	}
 	
-
 	public String getFirstName() {
 		return firstName;
 	}
 	
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 	
-
 	public String getLastName() {
 		return lastName;
 	}
 	
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
-
 	public String getEmail() {
 		return email;
-	}
-	
+	}	
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
-
 	public String getPassword() {
 		return password;
 	}
 	
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -122,6 +119,38 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 	
